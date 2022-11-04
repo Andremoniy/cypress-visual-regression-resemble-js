b176db7d4f0cc692ed2bb0c53f9b49b8dd45573f
Andrey Lebedev <andremoniy@gmail.com>
2022-11-04 12:16:13 +0100

    Create utils-browser.js

---
+++ src/utils-browser.js
@@ -0,0 +1,12 @@
+class CompareSnapshotsPluginError extends Error {
+  constructor(error) {
+    super(error.message);
+    Object.getOwnPropertyNames(error).forEach(prop => {
+      this[prop] = error[prop];
+    });
+  }
+}
+
+const deserializeError = error => new CompareSnapshotsPluginError(JSON.parse(error));
+
+module.exports = { deserializeError };
