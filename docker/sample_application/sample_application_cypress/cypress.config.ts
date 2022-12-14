499b15dec723291db7d95c766199b313043104af
andremoniy2 <n/a>
2022-12-14 20:30:07 +0100

    Sample application

--- docker/sample_application/sample_application_cypress/cypress.config.ts
+++ docker/sample_application/sample_application_cypress/cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-import * as getCompareSnapshotsPlugin from "cypress-visual-regression/dist/plugin";
+import * as getCompareSnapshotsPlugin from "cypress-visual-regression-ressemble-js/dist/plugin";
 
 export default defineConfig({
   e2e: {
--- docker/sample_application/sample_application_cypress/cypress/support/commands.ts
+++ docker/sample_application/sample_application_cypress/cypress/support/commands.ts
@@ -1,4 +1,4 @@
-import * as compareSnapshotCommand from "cypress-visual-regression/dist/command";
+import * as compareSnapshotCommand from "cypress-visual-regression-ressemble-js/dist/command";
 
 // @ts-ignore
 compareSnapshotCommand();
