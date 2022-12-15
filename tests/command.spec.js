96adbdf70b61e51dff3a3f810ec2537a8c4821ce
Gregory <gregory.delfrate@gmail.com>
2022-12-15 22:02:27 +0100

    feat: [CVRRJ-2] increase test coverage run npm install after moving deps

--- tests/command.spec.js
+++ tests/command.spec.js
@@ -1,5 +1,5 @@
 global.Cypress = {
-  env: () => false,
+  env: jest.fn(),
   log: () => null,
   config: () => "/cypress/screenshots",
   Commands: {
@@ -7,13 +7,18 @@ global.Cypress = {
   },
 };
 
-const compareSnapshotCommand = require("../src/command");
+
+global.cy = {
+  get: jest.fn(),
+  screenshot: jest.fn()
+}
+const command = require("../src/command");
 
 describe("compareSnapshot command", () => {
   it("should be added", () => {
     Cypress.Commands.add.mockReset();
 
-    compareSnapshotCommand();
+    command.compareSnapshotCommand();
 
     expect(Cypress.Commands.add).toHaveBeenCalledWith(
       "compareSnapshot",
@@ -21,4 +26,12 @@ describe("compareSnapshot command", () => {
       expect.any(Function)
     );
   });
+
+  it("should get cypress env variables", () => {
+    command.compareSnapshot()(undefined, "test-name")
+
+    expect(Cypress.env).toHaveBeenCalledWith('SNAPSHOT_BASE_DIRECTORY')
+    expect(Cypress.env).toHaveBeenCalledWith('SNAPSHOT_DIFF_DIRECTORY')
+    expect(Cypress.env).toHaveBeenCalledWith('ALWAYS_GENERATE_DIFF')
+  })
 });
