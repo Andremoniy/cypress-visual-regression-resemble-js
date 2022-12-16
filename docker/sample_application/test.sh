c846119326b4c6762292e6c9598eb0088fbbe6d0
andremoniy2 <n/a>
2022-12-16 17:43:04 +0100

    Sample application

--- docker/sample_application/test.sh
+++ docker/sample_application/test.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 npm i
 npm start&
+
+health_check() {
+  echo "Waiting NPM server to start..."
+  until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+      printf '.'
+      sleep 5
+  done
+  echo "Server started..."
+}
+
+health_check
+echo "Killing npm server..."
+pkill -f node
+npm start&
+health_check
 cd sample_application_cypress
 npm i
 npm run cypress:run
+
