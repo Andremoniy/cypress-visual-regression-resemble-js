2be186ad888a2b7fde5442444eb845072443e867
andremoniy2 <n/a>
2022-12-16 18:08:30 +0100

    Sample application

--- docker/sample_application/sample_application_cypress/cypress/integration/Regression.spec.ts
+++ docker/sample_application/sample_application_cypress/cypress/integration/Regression.spec.ts
@@ -3,6 +3,8 @@
 describe("Testing Regression", () => {
   it("takes a snapshot", () => {
     cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
     cy.viewport(1000, 660);
     cy.compareSnapshot("test", 0.07);
   });
