5fa3e8631901a2a11ebb4694e8d765aeb737a75a
andremoniy2 <n/a>
2023-01-14 11:08:02 +0100

    Delete extra steps in the docker test

--- docker/sample_application/sample_application_cypress/cypress/integration/Regression.spec.ts
+++ docker/sample_application/sample_application_cypress/cypress/integration/Regression.spec.ts
@@ -3,6 +3,7 @@
 describe("Testing Regression", () => {
   it("takes a snapshot", () => {
     cy.visit("/");
+    cy.wait(1000);
     cy.viewport(1000, 660);
     cy.compareSnapshot("test", 0.065);
   });
